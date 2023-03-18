5e5720574d895e487a28b1a3e0bb06a1a612778f
Thorsten Ludewig <t.ludewig@gmail.com>
2023-03-18 15:57:04 +0100

    start latest

--- START_LATEST.sh
+++ START_LATEST.sh
@@ -1,9 +1,3 @@
-# 389ds-fedora
-Build multi architecture Docker images for 389ds on Fedora
 
-### docker run
-
-```bash
 docker run --name 389ds-fedora --rm -p 3389:3389 -p 3636:3636 \
   -v `pwd`/data:/data ghcr.io/thorsten-l/389ds-fedora:latest
-```
