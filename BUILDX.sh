c97a85e96e0ce8d7039a9f75244b64753cf00239
Thorsten Ludewig <t.ludewig@gmail.com>
2023-03-18 15:48:15 +0100

    build xxx:latest version also

--- BUILDX.sh
+++ BUILDX.sh
@@ -2,13 +2,25 @@
 
 OS_VERSION=$1
 DS_VERSION=$2
+LATEST=$3
 echo "OS_VERSION=$OS_VERSION"
 echo "DS_VERSION=$DS_VERSION"
 
-docker buildx build --no-cache \
-  --build-arg OS_VERSION="$OS_VERSION" \
-  --build-arg DS_VERSION="$DS_VERSION" \
-  --push \
-  --platform linux/arm64,linux/amd64 \
-  --tag "ghcr.io/thorsten-l/389ds-fedora:$DS_VERSION" \
-  --tag "ghcr.io/thorsten-l/389ds-fedora:$DS_VERSION-fc$OS_VERSION" .
+if [ "true" = "$LATEST" ]; then
+  docker buildx build --no-cache \
+    --build-arg OS_VERSION="$OS_VERSION" \
+    --build-arg DS_VERSION="$DS_VERSION" \
+    --push \
+    --platform linux/arm64,linux/amd64 \
+    --tag "ghcr.io/thorsten-l/389ds-fedora:$DS_VERSION-fc$OS_VERSION" \
+    --tag "ghcr.io/thorsten-l/389ds-fedora:$DS_VERSION" \
+    --tag "ghcr.io/thorsten-l/389ds-fedora:latest" .
+else
+  docker buildx build --no-cache \
+    --build-arg OS_VERSION="$OS_VERSION" \
+    --build-arg DS_VERSION="$DS_VERSION" \
+    --push \
+    --platform linux/arm64,linux/amd64 \
+    --tag "ghcr.io/thorsten-l/389ds-fedora:$DS_VERSION-fc$OS_VERSION" \
+    --tag "ghcr.io/thorsten-l/389ds-fedora:$DS_VERSION" .
+fi
--- BUILDX_VERSIONS.sh
+++ BUILDX_VERSIONS.sh
@@ -1,4 +1,4 @@
 
 ./BUILDX.sh 36 2.1.7
 ./BUILDX.sh 37 2.2.6
-./BUILDX.sh 38 2.3.2
+./BUILDX.sh 38 2.3.2 "true"
